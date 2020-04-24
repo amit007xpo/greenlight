feb49ff69ad50f8be321dcc5314401d4c98aa22c
amit_edbot <amitk@mirafra.com>
2020-04-24 16:29:11 +0530

    custom-changes: final changes-8

--- config/application.rb
+++ config/application.rb
@@ -130,10 +130,10 @@ module Greenlight
     # DEFAULTS
 
     # Default branding image if the user does not specify one
-    config.branding_image_default = "https://github.com/amit007xpo/greenlight/blob/custom-changes/app/assets/images/logo_with_text.jpeg"
+    config.branding_image_default = "https://raw.githubusercontent.com/amit007xpo/greenlight/custom-changes/app/assets/images/logo_with_text.jpeg"
 
     #Default fevicon 
-    config.default_fevicon = "fevicon.jpeg"
+    config.default_fevicon = "https://raw.githubusercontent.com/amit007xpo/greenlight/custom-changes/app/assets/images/fevicon.jpeg"
     # Default primary color if the user does not specify one
     config.primary_color_default = "#467fcf"
 
